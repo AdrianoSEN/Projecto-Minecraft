c4f7f22235455ddb80891f00e1615cfe2b676d32
Adriano.S <asenmache@crackthecode.la>
2022-11-15 20:12:41 -0500

    Agregando mis archivos html y css

---
+++ script.js
@@ -0,0 +1,42 @@
+// Variables
+// Condicionales
+// Bucles
+
+
+// imprimir nuestra variable
+
+
+
+
+// Creen variables para su nombre, apellido, edad, altura y peso.
+// Imprimir cada una de estas variables
+let nombre = "Adriano Senmache";
+let edad = 24;
+let altura = 1.69; // esta en metros
+
+
+console.log(nombre)
+console.log(edad)
+console.log(altura)
+
+
+// Operaciones matematicas
+let a = 50
+let b = 30
+let c = 70
+
+let resultado1 = 0
+let resultado2 = 0
+let resultado3 = 0
+let resultado4 = 0 
+
+resultado1 = 
+resultado2 = 
+resultado3 = 
+resultado4 = 
+
+console.log(resultado1)
+
+
+// Creen 4 operaciones combinadas usando los numeros, variables y
+// operaciones que deseen
